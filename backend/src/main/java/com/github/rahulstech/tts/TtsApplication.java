98183a931c56b0101a28ad17496d8b86fa0621d0
Rahul Bagchi <rahulstech18@gmail.com>
2026-09-09 06:19:18 +0530

    backend project created

---
+++ backend/src/main/java/com/github/rahulstech/tts/TtsApplication.java
@@ -0,0 +1,13 @@
+package com.github.rahulstech.tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/rahulstech/tts/TtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rahulstech.tts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
